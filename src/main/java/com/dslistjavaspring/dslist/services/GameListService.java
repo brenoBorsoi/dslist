4cd9f2146d83879b0ad3913a135650810dd2e40b
Breno Borsoi <brenoborsoi@gmail.com>
2023-11-18 17:39:08 -0300

    Move games on list

--- src/main/java/com/dslistjavaspring/dslist/services/GameListService.java
+++ src/main/java/com/dslistjavaspring/dslist/services/GameListService.java
@@ -5,6 +5,7 @@ import com.dslistjavaspring.dslist.dto.GameListDTO;
 import com.dslistjavaspring.dslist.dto.GameMinDTO;
 import com.dslistjavaspring.dslist.entities.Game;
 import com.dslistjavaspring.dslist.entities.GameList;
+import com.dslistjavaspring.dslist.projections.GameMinProjection;
 import com.dslistjavaspring.dslist.repositories.GameListRepository;
 import com.dslistjavaspring.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,31 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
        List<GameList> result = gameListRepository.findAll();
        return result.stream().map(x -> new GameListDTO(x)).toList();
     }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex <destinationIndex ? destinationIndex : sourceIndex;
+
+        for (int i = min; i <= max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+
+        }
+
+    }
+
 }
